eb129314a502f066de94c0c809b87429b767b97d
pglutz <pglutz@berkeley.edu>
2023-02-03 14:29:39 -0800

    prefix-open quasi-determinacy mostly proved

--- src/games.lean
+++ src/games.lean
@@ -1,20 +1,19 @@
 import tactic
 import prefixes
 
-class game (α : Type*):=
-(players : Type*)
-(turn : (list α) → players)
+class game (α β : Type*):=
+(turn : (list α) → β)
 
-variables {α : Type*}
+variables {α β : Type*}
 
-class quasi_strategy (G : game α) :=
-(player : G.players)
+class quasi_strategy (G : game α β) :=
+(player : β)
 (positions : set (list α))
 (is_quasi_strategy : ∀ t : list α, t ∈ positions →
   (G.turn(t) = player → ∃ a, t.concat a ∈ positions) ∧
   (G.turn(t) ≠ player → ∀ a, t.concat a ∈ positions))
 
-variables {G : game α} (σ : quasi_strategy G) (X : (ℕ → α) → G.players) 
+variables {G : game α β} (σ : quasi_strategy G) (X : (ℕ → α) → β) 
   (s : list α) (f : ℕ → α)
 
 def s_quasi_strategy :=
@@ -28,33 +27,32 @@ def is_strategy :=
 
 def is_play := ∃ N, ∀ n ≥ N, stream_prefix f n ∈ σ.positions
 
-def is_winning := ∀ f : ℕ → α, is_play σ f → X f = σ.player
+def winning := ∀ f : ℕ → α, is_play σ f → X f = σ.player
 
-def is_s_winning := s_quasi_strategy σ s ∧ is_winning σ X
+def s_winning := s_quasi_strategy σ s ∧ winning σ X
 
-def quasi_determined := ∃ σ : quasi_strategy G, is_s_winning σ X s 
+def quasi_determined := ∃ σ : quasi_strategy G, s_winning σ X s 
 
 def determined := ∃ σ : quasi_strategy G, 
-  is_s_winning σ X s ∧ is_strategy σ
+  s_winning σ X s ∧ is_strategy σ
 
-variables (p : G.players)
-
-def winning_position := ∃ σ : quasi_strategy G, 
-  is_s_winning σ X s ∧ σ.player = p
-
-variables {γ : Type*}
+variables (p : β) {γ : Type*}
 
 instance union_of_quasi_strategies (g : γ → quasi_strategy G)
   (hg : ∀ x, (g x).player = p) : quasi_strategy G :=
 ⟨p, ⋃ (x : γ), (g x).positions, sorry⟩
 
+lemma mem_union_of_mem (g : γ → quasi_strategy G) (hg : ∀ x, (g x).player = p)
+  (x : γ) (hs : s ∈ (g x).positions) : s ∈ (union_of_quasi_strategies p g hg).positions :=
+  set.mem_Union_of_mem x hs
+
 instance extension_of_quasi_strategy (a : α) (h : G.turn(s) = σ.player) 
   (h' : s.concat a ∈ σ.positions) : quasi_strategy G :=
 ⟨σ.player, {s} ∪ σ.positions, sorry⟩
 
-instance extension_of_quasi_strategy' (a : α)
-  (h : ∀ a, s.concat a ∈ σ.positions) : quasi_strategy G :=
-⟨σ.player, {s} ∪ σ.positions, sorry⟩
+-- instance extension_of_quasi_strategy'
+--   (h : ∀ a, s.concat a ∈ σ.positions) : quasi_strategy G :=
+-- ⟨σ.player, {s} ∪ σ.positions, sorry⟩
 
 lemma s_quasi_strategy_union (g : α → quasi_strategy G) (hg : ∀ a, (g a).player = p)
   (hg' : ∀ a, s_quasi_strategy (g a) (s.concat a)) :
@@ -64,56 +62,54 @@ lemma s_quasi_strategy_extension (a : α) (h : G.turn(s) = σ.player)
   (h' : s_quasi_strategy σ (s.concat a)) :
   s_quasi_strategy (extension_of_quasi_strategy σ s a h (s_in_s_quasi_strategy σ _ h')) s := sorry
 
-lemma s_quasi_strategy_extension' (a : α) (h : ∀ a, s.concat a ∈ σ.positions)
-  (h' : ∀ t, t ∈ σ.positions → s <+: t) :
-  s_quasi_strategy (extension_of_quasi_strategy' σ s a h) s := sorry
+lemma winning_quasi_strategy_union (g : α → quasi_strategy G) (hg : ∀ a, (g a).player = p)
+  (hg' : ∀ a, s_quasi_strategy (g a) (s.concat a)) (hg'' : ∀ a, winning (g a) X) :
+  winning (union_of_quasi_strategies p g hg) X := sorry
 
-lemma is_winning_quasi_strategy_union (g : α → quasi_strategy G) (hg : ∀ a, (g a).player = p)
-  (hg' : ∀ a, s_quasi_strategy (g a) (s.concat a)) (hg'' : ∀ a, is_winning (g a) X) :
-  is_winning (union_of_quasi_strategies p g hg) X := sorry
+lemma winning_quasi_strategy_extension (a : α) (h : G.turn(s) = σ.player)
+  (h' : s.concat a ∈ σ.positions) (h'' : winning σ X) :
+  winning (extension_of_quasi_strategy σ s a h h') X := sorry
 
-lemma is_winning_quasi_strategy_extension (a : α) (h : G.turn(s) = σ.player)
-  (h' : s.concat a ∈ σ.positions) (h'' : is_winning σ X) :
-  is_winning (extension_of_quasi_strategy σ s a h h') X := sorry
+lemma s_winning_quasi_strategy_union (g : α → quasi_strategy G) (hg : ∀ a, (g a).player = p)
+  (hg' : ∀ a, s_quasi_strategy (g a) (s.concat a)) (hg'' : ∀ a, winning (g a) X) :
+  s_winning (union_of_quasi_strategies p g hg) X s :=
+  ⟨s_quasi_strategy_union _ _ _ _ hg', winning_quasi_strategy_union _ s _ _ _ hg' hg''⟩
 
-lemma is_winning_quasi_strategy_extension' (a : α) (h : ∀ a, s.concat a ∈ σ.positions)
-  (h' : is_winning σ X) : is_winning (extension_of_quasi_strategy' σ s a h) X := sorry
+lemma s_winning_quasi_strategy_extension (a : α) (h : G.turn(s) = σ.player)
+  (h' : s_quasi_strategy σ (s.concat a)) (h'' : winning σ X) :
+  s_winning (extension_of_quasi_strategy σ s a h (s_in_s_quasi_strategy σ _ h')) X s :=
+  ⟨s_quasi_strategy_extension _ _ _ _ _, winning_quasi_strategy_extension _ _ _ _ _ _ h''⟩
 
-lemma is_s_winning_quasi_strategy_union (g : α → quasi_strategy G) (hg : ∀ a, (g a).player = p)
-  (hg' : ∀ a, s_quasi_strategy (g a) (s.concat a)) (hg'' : ∀ a, is_winning (g a) X) :
-  is_s_winning (union_of_quasi_strategies p g hg) X s :=
-  ⟨s_quasi_strategy_union _ _ _ _ hg', is_winning_quasi_strategy_union _ s _ _ _ hg' hg''⟩
-
-lemma is_s_winning_quasi_strategy_extension (a : α) (h : G.turn(s) = σ.player)
-  (h' : s_quasi_strategy σ (s.concat a)) (h'' : is_winning σ X) :
-  is_s_winning (extension_of_quasi_strategy σ s a h (s_in_s_quasi_strategy σ _ h')) X s :=
-  ⟨s_quasi_strategy_extension _ _ _ _ _, is_winning_quasi_strategy_extension _ _ _ _ _ _ h''⟩
-
-lemma is_s_winning_quasi_strategy_extension' (a : α) (h : ∀ a, s.concat a ∈ σ.positions)
-  (h' : ∀ t, t ∈ σ.positions → s <+: t) (h'' : is_winning σ X) :
-  is_s_winning (extension_of_quasi_strategy' σ s a h) X s :=
-  ⟨s_quasi_strategy_extension' _ _ _ _ h', is_winning_quasi_strategy_extension' _ _ _ _ _ h''⟩
-
-instance above_s_quasi_strategy [hα : nonempty α] : quasi_strategy G :=
+instance above_s_quasi_strategy (G : game α β) (s : list α) (p : β) [hα : nonempty α] : quasi_strategy G :=
 ⟨p, {t | s <+: t}, sorry⟩
 
 lemma above_s_is_s_quasi_strategy [hα : nonempty α] :
-  s_quasi_strategy (above_s_quasi_strategy s p) s := sorry
+  s_quasi_strategy (above_s_quasi_strategy G s p) s := sorry
 
 lemma above_s_winning_iff [hα : nonempty α] :
-  is_winning (above_s_quasi_strategy s p) X ↔ ∀ f, is_prefix s f → X f = p :=
+  winning (above_s_quasi_strategy G s p) X ↔ ∀ f, is_prefix s f → X f = p :=
   sorry
+
+instance quasi_strategy_restriction : quasi_strategy G :=
+⟨σ.player, {t ∈ σ.positions | s <+: t}, sorry⟩
+
+lemma s_quasi_strategy_restriction (h : s ∈ σ.positions) : s_quasi_strategy (quasi_strategy_restriction σ s) s :=
+  sorry
+
+lemma winning_restriction (h : winning σ X) : winning (quasi_strategy_restriction σ s) X :=
+  sorry
+
+lemma s_winning_restriction (h : s ∈ σ.positions) (h' : winning σ X) : 
+  s_winning (quasi_strategy_restriction σ s) X s :=
+  ⟨s_quasi_strategy_restriction _ _ h, winning_restriction _ _ _ h'⟩
 
 -- TODO:
 -- union of winning quasi strategies is winning (with appropriate extra hypothesis)
 -- extension of winning quasi strategy is winning
--- extension' of winning quasi strategies is winning
+-- restriction of quasi strategy to above s
 -- quasi strategy above s
 -- quasi strategy above s is winning iff
--- idea: prove determinacy for games where players = Prop?
--- define non-winning quasi-strategy
--- prove non-winning quasi-strategy is winning for player 2
--- prove quasi-determinacy
 -- strategy from quasi-strategy (instance of quasi-strategy)
 -- strategy is winning if quasi-strategy is winning
 -- prove determinacy
+-- prove open → is_prefix_open
--- src/prefixes.lean
+++ src/prefixes.lean
@@ -15,7 +15,42 @@ begin
   { rw [stream_prefix, list.length_concat, ih], },
 end
 
+lemma stream_prefix_prefix (f : ℕ → α) (n m : ℕ) (h : n ≤ m) :
+  stream_prefix f n <+: stream_prefix f m :=
+begin
+  induction m with m ih,
+  { rw nonpos_iff_eq_zero.mp h, },
+  { rw le_iff_eq_or_lt at h,
+    cases h,
+    { rw h, },
+    { rw nat.lt_succ_iff at h,
+       calc stream_prefix f n <+: stream_prefix f m : ih h 
+       ... <+: stream_prefix f m.succ : list.prefix_concat (f m) (stream_prefix f m), }, },
+end
+
 def is_prefix (s : list α) (f : ℕ → α) := stream_prefix f (s.length) = s
+
+lemma prefix_of_is_prefix (s : list α) (f : ℕ → α) (n : ℕ) (h : is_prefix s f)
+  (h' : n ≥ s.length) : s <+: stream_prefix f n :=
+begin
+  rw is_prefix at h,
+  rw ← h,
+  exact stream_prefix_prefix f s.length n h',
+end
+
+lemma is_prefix_of_prefix (s : list α) (f : ℕ → α) (n : ℕ)
+  (h : s <+: stream_prefix f n) : is_prefix s f :=
+begin
+  have hn : n ≥ s.length,
+  { rw ← (stream_prefix_length f n),
+    exact list.is_prefix.length_le h, },
+  have hf : (stream_prefix f s.length).length = s.length :=
+    stream_prefix_length f s.length,
+  apply list.eq_of_prefix_of_length_eq,
+  { apply list.prefix_of_prefix_length_le (stream_prefix_prefix f s.length n hn) h,
+    rw stream_prefix_length, },
+  { exact hf, },
+end
 
 def prefix_open (X : set (ℕ → α)) (C : set (list α)):= ∀ f : ℕ → α,
   f ∈ X ↔ ∃ s ∈ C, is_prefix s f 
